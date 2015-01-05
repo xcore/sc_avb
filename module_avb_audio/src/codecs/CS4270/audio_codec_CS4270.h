2a063aedad8e7cca342d3c20b6ec69535909f9ef
Andy Lucas <andy@xmos.com>
2015-01-05 22:37:15 +0000

    Moves CS4270 register defines into header for wider use

--- module_avb_audio/src/codecs/CS4270/audio_codec_CS4270.h
+++ module_avb_audio/src/codecs/CS4270/audio_codec_CS4270.h
@@ -3,6 +3,15 @@
 #include "avb_conf.h"
 #include "i2c.h"
 
+#define CODEC_DEV_ID_ADDR           0x01
+#define CODEC_PWR_CTRL_ADDR         0x02
+#define CODEC_MODE_CTRL_ADDR        0x03
+#define CODEC_ADC_DAC_CTRL_ADDR     0x04
+#define CODEC_TRAN_CTRL_ADDR        0x05
+#define CODEC_MUTE_CTRL_ADDR        0x06
+#define CODEC_DACA_VOL_ADDR         0x07
+#define CODEC_DACB_VOL_ADDR         0x08
+
 void audio_codec_CS4270_init(out port p_codec_reset,
                               int mask,
                               int codec_addr,
